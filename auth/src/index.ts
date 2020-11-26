b689ccc64f3c35a5be398e601b016648547ef3fd
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-11-26 13:25:14 -0600

    Add ingress to expose internal services

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
-import express from 'express';
-import { json } from 'body-parser';
+import express from "express";
+import { json } from "body-parser";
 
 const app = express();
-app.use(json())
+app.use(json());
+
+app.get("api/users/current", (_, res) => {
+  res.send({ message: "Hi, there." });
+});
 
 app.listen(8000, () => {
-  console.log('Listening on port: 8000')
-})
+  console.log("Listening on port: 8000!!!");
+});
