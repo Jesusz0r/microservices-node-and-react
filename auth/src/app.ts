a0bc55ac3f0c620fc1384b7d606b685e1a759bb7
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-11-30 11:58:29 -0600

    Setup initial tests config

---
+++ auth/src/app.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import "express-async-errors";
+import { json, urlencoded } from "body-parser";
+import morgan from "morgan";
+import cookieSession from "cookie-session";
+
+import { authRoutes } from "./routes";
+import { errorHandler } from "./middlewares";
+import { NotFoundError } from "./errors";
+
+const app = express();
+
+app.set("trust proxy", true);
+app.use(json());
+app.use(urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    name: "session",
+    signed: false,
+    secure: true,
+  })
+);
+app.use(morgan("dev"));
+
+app.use("/api/users", authRoutes);
+app.use(errorHandler);
+
+app.all("*", (req, res, next) => {
+  const notFoundError = new NotFoundError();
+  const errors = notFoundError.serializeErrors();
+
+  res.status(notFoundError.statusCode).send({ errors });
+});
+
+export { app };
--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,54 +1,26 @@
-import express from "express";
-import "express-async-errors";
-import { json, urlencoded } from "body-parser";
-import morgan from "morgan";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
 
-import { authRoutes } from "./routes";
-import { errorHandler } from "./middlewares";
-import { NotFoundError } from "./errors";
-
-const app = express();
-
-app.set("trust proxy", true);
-app.use(json());
-app.use(urlencoded({ extended: true }));
-app.use(
-  cookieSession({
-    name: "session",
-    signed: false,
-    secure: true,
-  })
-);
-app.use(morgan("dev"));
-
-app.use("/api/users", authRoutes);
-app.use(errorHandler);
-
-app.all("*", (req, res, next) => {
-  const notFoundError = new NotFoundError();
-  const errors = notFoundError.serializeErrors();
-
-  res.status(notFoundError.statusCode).send({ errors });
-});
+import { app } from "./app";
 
 if (!process.env.JWT_SECRET) {
-  throw new Error("");
+  throw new Error("JWT_SECRET environment variable is required.");
 }
 
-app.listen(8000, async () => {
-  console.log("Listening on port: 8000!");
-
+async function start() {
   try {
+    await app.listen(8000);
     await mongoose.connect("mongodb://auth-mongo-service:27017/auth", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
+
+    console.log("Server is up and running on port: 8000");
     console.log("Connected to the database!");
   } catch (error) {
     console.error(error.message);
     process.exit(1);
   }
-});
+}
+
+start();
