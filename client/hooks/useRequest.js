b4a565f9023e200ca55a2ca12675aec1af7ad207
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-01 17:42:56 -0600

    Move errors logic to useRequest hook, add a onSuccess and onFailure callbacks

--- client/hooks/useRequest.js
+++ client/hooks/useRequest.js
@@ -1,9 +1,12 @@
 import { useState } from "react";
 import axios from "axios";
 
-const useRequest = (url, { method = "GET", ...options }) => {
+const useRequest = (
+  url,
+  { method = "GET", onSuccess = () => {}, onFailure = () => {}, ...options }
+) => {
   const [data, setData] = useState(null);
-  const [errors, setErrors] = useState([]);
+  const [errors, setErrors] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const doRequest = async () => {
@@ -13,8 +16,10 @@ const useRequest = (url, { method = "GET", ...options }) => {
       const response = await axios({ url, method, ...options });
 
       setData(response.data);
+      onSuccess(response.data);
     } catch (err) {
       setErrors(err.response.data.errors);
+      onFailure();
     } finally {
       setLoading(false);
     }
