d584e516e34ed69d61e308b29eab4849e7ea8801
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-01 17:44:04 -0600

    request current user on route load

---
+++ client/api/index.js
@@ -0,0 +1,1 @@
+export { buildAxiosClient } from "./build-axios-client";
--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,7 +1,36 @@
 import "bootstrap/dist/css/bootstrap.css";
+import { buildAxiosClient } from "../api";
 
-const App = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+import Header from "../components/Header";
+
+const _App = ({ Component, user, pageProps }) => {
+  return (
+    <>
+      <Header user={user} />
+      <Component {...pageProps} user={user} />
+    </>
+  );
 };
 
-export default App;
+_App.getInitialProps = async ({ ctx, Component }) => {
+  const axios = buildAxiosClient(ctx);
+  const pageProps = Component.getInitialProps
+    ? await Component.getInitialProps(ctx)
+    : null;
+
+  try {
+    const response = await axios.get("/api/users/current");
+
+    return {
+      pageProps,
+      ...response.data,
+    };
+  } catch (error) {
+    return {
+      pageProps,
+      user: {},
+    };
+  }
+};
+
+export default _App;
