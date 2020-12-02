ddce3265ff8e1f919598e922212c6ae30473e1b1
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-02 13:14:38 -0600

    Check if user exists on database before validating

--- auth/src/middlewares/verify-user.ts
+++ auth/src/middlewares/verify-user.ts
@@ -2,6 +2,8 @@ import { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { NotAuthorizedError } from "@encuentradepa/common";
 
+import { User } from "../models";
+
 interface UserPayload {
   id: string;
   email: string;
@@ -14,7 +16,11 @@ declare global {
   }
 }
 
-export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
+export const verifyUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const token = req.session?.jwt || req.headers?.authorization?.split(" ")[1];
 
   if (!token) {
@@ -23,6 +29,11 @@ export const verifyUser = (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const user = jwt.verify(token, process.env.JWT_SECRET!) as UserPayload;
+    const userExists = await User.findOne({ _id: user.id });
+
+    if (!userExists) {
+      throw new Error();
+    }
 
     req.user = user;
   } catch (error) {
