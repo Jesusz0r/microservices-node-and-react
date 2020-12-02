56fef3df54484689c6b946b0fec0ded030bc93ec
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-02 15:43:17 -0600

    Setup tests

---
+++ tickets/src/routes/__tests__/new-ticket.test.ts
@@ -0,0 +1,11 @@
+import request from "supertest";
+
+import { app } from "app";
+
+it("should create a new ticket succesfuly", async () => {});
+
+it("should not create a new ticket if there is not an user authenticated", async () => {});
+
+it("should not create a new ticket if invalid title is provided", async () => {});
+
+it("should not create a new ticket if price title is provided", async () => {});
