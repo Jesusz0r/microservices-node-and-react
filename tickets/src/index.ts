d082a06ae67fc9bb580af407d022a5a7bde3a3fd
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-07 13:30:01 -0600

    Connect to nats using the natsWrapper

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 
 import { app } from "./app";
+import { natsWrapper } from "./nats-wrapper";
 
 async function start() {
   try {
@@ -9,6 +10,19 @@ async function start() {
     }
 
     await app.listen(8000);
+
+    const nats = await natsWrapper.connect(
+      "ticketing",
+      "tickets123",
+      "http://nats-service:4222"
+    );
+    nats.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit(0);
+    });
+    process.on("SIGINT", () => nats.close());
+    process.on("SIGTERM", () => nats.close());
+
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
