01359987311152ae5327bcd227e61806ca8fe19b
Jesus Mendoza <jesusreveron1990@gmail.com>
2020-12-09 20:46:02 -0600

    Set up tests

---
+++ orders/src/tests/setup.ts
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+import { MongoMemoryServer } from "mongodb-memory-server";
+
+let mongo: any;
+
+jest.mock("../nats-wrapper");
+jest.mock("../middlewares");
+
+beforeAll(async () => {
+  process.env.JWT_SECRET = "asdasd";
+
+  mongo = new MongoMemoryServer();
+
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  });
+});
+
+beforeEach(async () => {
+  await mongoose.connection.dropDatabase();
+});
+
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
